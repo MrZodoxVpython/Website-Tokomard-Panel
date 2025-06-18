4f3a403058a0da517290549bcbd4482d5dee964e
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-19 01:38:49 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-19 01:38:49

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <meta property="og:image" content="https://i.imgur.com/q3DzxiB.png">
   <meta property="og:url" content="https://panel.tokomard.store/">
   <meta property="og:type" content="website">
-  <title>Login - X-Panel</title>
+  <title>Login Tokomard</title>
   <link rel="SHORTCUT ICON" href="https://i.imgur.com/q3DzxiB.png">
   <!-- Tailwind CSS CDN -->
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
