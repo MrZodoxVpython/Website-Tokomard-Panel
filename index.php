ced79cbe7ec31b4993f7cd0cf01b52805465fa34
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-19 01:10:42 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-19 01:10:42

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login - X-Panel</title>
+  <link rel="SHORTCUT ICON" href="https://i.imgur.com/q3DzxiB.png">
   <!-- Tailwind CSS CDN -->
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
