8495de580cfb6354d3481d2d3443b475e62b639b
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-19 07:45:42 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-19 07:45:42

--- kelola-akun.php
+++ kelola-akun.php
@@ -262,9 +262,9 @@ include 'templates/header.php';
         <input type="text" name="expired" placeholder="2025-07-01 atau 30" class="w-full p-2 bg-gray-700 rounded" required>
       </div>
 <!-- Dropdown Pilihan VPS -->
-<div class="mb-4">
+<div class="mb-2">
   <label class="block text-sm font-medium text-white mb-1">Pilih VPS</label>
-  <select name="vps" class="w-full p-1 bg-gray-700 text-white rounded" required>
+  <select name="vps" class="w-full p-2 bg-gray-700 text-white rounded" required>
     <option value="rw-mard1">RW-MARD1</option>
     <option value="sgdo-mard1">SGDO-MARD1</option>
     <option value="sgdo-2dev">SGDO-2DEV</option>
