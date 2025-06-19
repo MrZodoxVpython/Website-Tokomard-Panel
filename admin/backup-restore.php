ebe14dcd6dea1878afd64f50f8dc95d47333c76b
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-19 13:37:02 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-19 13:37:02

--- admin/backup-restore.php
+++ admin/backup-restore.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
-    header("Location: index.php");
+    header("Location: /index.php");
     exit;
 }
 
