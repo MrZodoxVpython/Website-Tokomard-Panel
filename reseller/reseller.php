a137aaab3ef46ba5ef9594580a9e7a35a243f21a
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-22 14:36:15 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-22 14:36:15

--- reseller/reseller.php
+++ reseller/reseller.php
@@ -68,7 +68,7 @@ $vpsList = [
             <button onclick="toggleTheme()" class="px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-500 text-sm">
                 Ganti Tema
             </button>
-            <a href="logout.php" class="px-4 py-2 bg-red-600 rounded hover:bg-red-500">
+            <a href="logout.php" class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-500">
                 Logout
             </a>
         </div>
