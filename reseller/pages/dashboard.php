58cc43156d644d57519c7385f40521a973ac673c
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-22 15:38:06 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-22 15:38:06

--- reseller/pages/dashboard.php
+++ reseller/pages/dashboard.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
-if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
-    header("Location: /index.php");
-    exit;
-}
 
 $loggedInUser = [
     'username' => $_SESSION['username'],
