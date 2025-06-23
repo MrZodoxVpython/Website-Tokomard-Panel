c6805113c6ac86677419c76cd7af33cb5e7c7e5b
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-23 09:34:01 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-23 09:34:01

--- admin/backup-restore.php
+++ admin/backup-restore.php
@@ -33,11 +33,7 @@ include 'templates/header.php';
 
   <!-- Menu Form -->
   <form method="POST" class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-8">
-    <button type="submit" name="action" value="backup"
-      class="bg-green-600 hover:bg-green-700 px-6 py-4 rounded-xl text-white text-xl shadow text-center">
-      <a href="bg-green-600 hover:bg-green-700 px-6 py-4 rounded-xl text-white text-xl shadow text-center auto-instal-rclone.php">>🗃 Launch Backup</a>
-    </button>
-
+      <a href="auto-install-rclone.php" class="bg-green-600 hover:bg-green-700 px-6 py-4 rounded-xl text-white text-xl shadow text-center">>🗃 Launch Backup</a>
     <button type="submit" name="action" value="restore"
       class="bg-yellow-500 hover:bg-yellow-600 px-6 py-4 rounded-xl text-white text-xl shadow text-center">
       ♻️ Restore dari Backup
