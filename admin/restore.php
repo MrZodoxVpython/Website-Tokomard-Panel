2b2e19557015f412b2ec53bfb4b72cd04e13f061
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-23 11:57:53 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-23 11:57:53

--- admin/restore.php
+++ admin/restore.php
@@ -31,7 +31,7 @@ if (isset($_POST['mode'])) {
         $scriptContent = <<<EOL
 #!/bin/bash
 
-TOKEN_FILE="/var/tmp/token.json"
+TOKEN_FILE="/tmp/token.json"
 DEST="/root/backup-vpn.tar.gz"
 RESTORE_DIR="/root/backup-vpn"
 RCLONE_CONF="/root/.config/rclone/rclone.conf"
