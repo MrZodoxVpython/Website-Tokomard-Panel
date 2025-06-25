acc8dd6af715d6fe63095d8d73b205fe70d6e981
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-25 17:20:55 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-25 17:20:55

---
+++ reseller/pages/api-akun/add-ssh.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'lib-akun.php'; // File yang berisi fungsi hitungTanggalExpired()
+
+if ($argc < 4) {
+    echo "❌ Format: php tambah-akun-ssh.php username expired password\n";
+    exit(1);
+}
+$username = $argv[1];
+$expired  = hitungTanggalExpired($argv[2]);
+$key      = $argv[3];
+
+$cmd = "sudo useradd -e $expired -s /bin/false -M $username && echo \"$username:$key\" | sudo chpasswd";
+shell_exec($cmd);
+
+// Cetak hasil sama seperti logika di `tambah-akun.php`
+tampilkanSSH($username, $expired, $key);
+
