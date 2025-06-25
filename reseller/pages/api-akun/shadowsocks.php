315fa9caaafacdc1e26ec12dfbb93af7fd49b1d3
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-25 19:23:57 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-25 19:23:57

---
+++ reseller/pages/api-akun/shadowsocks.php
@@ -0,0 +1,19 @@
+<?php
+require_once '/lib-akun.php';
+
+if ($argc < 4) {
+    echo "❌ Parameter tidak lengkap!\n";
+    echo "Gunakan: php tambah-shadowsocks.php username expired password\n";
+    exit(1);
+}
+
+$username = $argv[1];
+$expired  = hitungTanggalExpired($argv[2]);
+$password = $argv[3];
+
+$commentLine = "#$ $username $expired";
+$jsonLine = "},{\"password\": \"$password\", \"method\": \"aes-128-gcm\", \"email\": \"$username\"";
+$tags = ['ssws', 'ssgrpc'];
+
+prosesXray('shadowsocks', $tags, $commentLine, $jsonLine, $username, $expired, $password);
+
