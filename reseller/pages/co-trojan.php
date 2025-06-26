ef228455fb3d8946a5d1b47e103d2fea4c3d9b7e
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-26 12:45:54 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-26 12:45:54

--- reseller/pages/co-trojan.php
+++ reseller/pages/co-trojan.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'reseller') {
+    echo "❌ Anda belum login sebagai reseller!";
     header("Location: ../index.php");
     exit;
 }
