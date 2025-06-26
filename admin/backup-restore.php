22cc4e815d0821898f6693cd84363f68478e07c7
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-27 00:57:36 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-27 00:57:36

--- admin/backup-restore.php
+++ admin/backup-restore.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($action === 'backup') {
         if ($isLocal) {
-            $cmd = "/admin/backup.php";
+            $cmd = "admin/backup.php";
         } elseif (!empty($password)) {
             $cmd = "sshpass -p '$password' ssh -o StrictHostKeyChecking=no root@$vpsIp 'sudo /usr/bin/backup'";
         } else {
