a7d2008fa2f6bc357de07f83a51fa1abe12d7cd4
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-26 12:44:28 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-26 12:44:28

--- reseller/pages/trojan.php
+++ reseller/pages/trojan.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'reseller') {
+    header("Location: ../index.php");
+    exit;
+}
+
 $sshProducts = [
     [
         'name' => 'SGDO-2DEV',
