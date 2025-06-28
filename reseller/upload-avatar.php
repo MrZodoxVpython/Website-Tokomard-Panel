c9d729ebb36d163c4a659706ecb4896906bc8489
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-29 00:46:20 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-29 00:46:20

--- reseller/upload-avatar.php
+++ reseller/upload-avatar.php
@@ -21,7 +21,7 @@ $size = $_FILES['avatar']['size'];
 // Validasi ekstensi
 $allowedExts = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
 if (!in_array($ext, $allowedExts)) {
-    tampilkanCyberpunkError("❌ Format gambar tidak didukung!.");
+    tampilkanCyberpunkError("❌ Format gambar tidak didukung!");
 }
 
 // Validasi ukuran
