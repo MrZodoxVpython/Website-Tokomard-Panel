d42d98a1c3f0f1207c16a08259f2ec3a9eab2f19
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-29 02:33:31 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-29 02:33:31

--- reseller/koneksi.php
+++ reseller/koneksi.php
@@ -1,7 +1,18 @@
 <?php
-$conn = new mysqli('127.0.0.1', 'benjamin', '', 'xray_db');
+$servername = "127.0.0.1";
+$username = "benjamin";
+$password = "wickman";
+$database = "xray_db";
+
+// Buat koneksi
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Cek koneksi
 if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+    die("❌ Koneksi gagal: " . $conn->connect_error);
 }
+
+// Koneksi berhasil
+// echo "✅ Koneksi berhasil!";
 ?>
 
