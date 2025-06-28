0fdefc9fecfe9a5c437b9158a81defefef6554e0
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-29 03:37:39 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-29 03:37:39

--- reseller/resellerOLD.php
+++ reseller/resellerOLD.php
@@ -106,9 +106,9 @@ if ($notifResult) {
 >
   <?= $notifCount > 9 ? '9+' : $notifCount ?>
 </span>
-    <?php endif; ?>
   <h1 class="text-xl font-bold select-none">Panel Reseller Tokomard</h1>
 
+    <?php endif; ?>
   </div>
   <div class="flex items-center gap-4">
     <button id="themeToggleBtn" class="text-xl p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700"><?= $theme === 'dark' ? '🌞' : '🌙' ?></button>
