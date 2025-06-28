df010ea0360ddbeca482ab4892afb95f2ef8a683
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-29 02:36:40 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-29 02:36:40

---
+++ reseller/pesan.php
@@ -0,0 +1,5 @@
+<form method="POST" action="kirim-notifikasi.php">
+  <textarea name="pesan" placeholder="Tulis pesan notifikasi..." required></textarea>
+  <button type="submit">Kirim</button>
+</form>
+
