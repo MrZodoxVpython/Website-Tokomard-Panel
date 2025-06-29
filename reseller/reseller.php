e44905a669627783eec48853f225f6052fa19fb9
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-29 13:31:14 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-29 13:31:14

--- reseller/reseller.php
+++ reseller/reseller.php
@@ -134,9 +134,9 @@ if ($notifResult) {
 
 <!-- Tombol Aksi -->
   <div class="flex items-center gap-4">
-<button id="themeToggleBtn" class="text-xl p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700">
-  <span id="themeIcon"><?= $theme === 'dark' ? '🌞' : '🌙' ?></span>
-</button>
+  <button onclick="toggleTheme()" id="themeToggleBtn" class="text-xl p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700">
+    <span id="themeIcon"><?= $theme === 'dark' ? '🌞' : '🌙' ?></span>
+  </button>
     <a href="../logout.php" class="px-3 py-2 bg-red-600 text-white rounded hover:bg-red-500 text-sm">
       Logout
     </a>
