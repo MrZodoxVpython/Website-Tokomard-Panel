e2dfd8f55a580862bdeae43ec4872ff786bc4cb3
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-02 05:02:58 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-02 05:02:58

--- reseller/resellerNO.php
+++ reseller/resellerNO.php
@@ -128,15 +128,14 @@ if ($stmt) {
 
   <!-- MAIN CONTENT -->
   <main class="flex-1 ml-0 md:ml-64 p-4">
-<?php
-$path = __DIR__ . "/pages/{$page}.php";
-if (file_exists($path)) {
-    include $path;
-} else {
-    echo "<div class='text-red-500'>Halaman <b>{$page}</b> tidak ditemukan.</div>";
-}
-?>
-
+  <?php
+  $path = __DIR__ . "/pages/{$page}.php";
+  if (file_exists($path)) {
+      include $path;
+  } else {
+      echo "<div class='text-red-500'>Halaman <b>{$page}</b> tidak ditemukan.</div>";
+  }
+  ?>
   </main>
 </div>
 
