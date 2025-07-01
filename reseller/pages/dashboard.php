24560d6ffd7815401a2cb3f6de3034ef8893354d
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-02 05:13:50 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-02 05:13:50

--- reseller/pages/dashboard.php
+++ reseller/pages/dashboard.php
@@ -14,7 +14,7 @@
   </style>
 </head>
 <body class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white">
-  <div class="container mx-auto px-4 py-6">
+  <div class="overflow-x-hidden w-full">
     <?php
     $reseller = $_SESSION['reseller'] ?? 'unknown'; // pastikan reseller tersedia
 
