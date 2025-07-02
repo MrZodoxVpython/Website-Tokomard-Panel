4da5157deee377beb0df304b99d4eda217b9f138
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-02 11:51:53 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-02 11:51:53

--- reseller/reseller.php
+++ reseller/reseller.php
@@ -148,7 +148,7 @@ if ($stmt) {
   </form>
 
   <!-- Username reseller + titik merah NOTIF -->
-  <div class="relative text-center font-semibold cursor-pointer -mt-2 mb-10" onclick="toggleNotif()">
+  <div class="relative text-center font-semibold cursor-pointer -mt-2 mb-8" onclick="toggleNotif()">
     @<?= htmlspecialchars($reseller) ?>
     <?php if ($notifCount > 0): ?>
     <span class="absolute -top-1 -right-2 w-4 h-4 bg-red-600 text-white text-[10px] flex items-center justify-center font-bold rounded-full animate-pulse z-10">
