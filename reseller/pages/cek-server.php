137fc3858d86cd0b51f8018c2e6156435f7c082e
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-04 18:41:32 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-04 18:41:32

--- reseller/pages/cek-server.php
+++ reseller/pages/cek-server.php
@@ -108,7 +108,7 @@ foreach ($servers as $name => $domain) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-900 text-white min-h-screen p-4 md:p-6 space-y-6">
+<body class="bg-gray-900 text-white min-h-screen p-6 md:p-6 space-y-6">
 
 <!-- STATUS SERVER -->
 <div class="max-w-6xl mx-auto bg-gray-800 rounded-lg p-4 md:p-6 shadow-lg mb-6">
