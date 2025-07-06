fcb2cd6338da5f5d3a9558da63ece1af9c5fc673
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-07 01:36:43 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-07 01:36:43

--- reseller/pages/show-trojan-rw-mard.php
+++ reseller/pages/show-trojan-rw-mard.php
@@ -1,9 +1,9 @@
 <?php
+session_start();
 ob_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
 
 $reseller = $_SESSION['reseller'] ?? $_SESSION['username'] ?? '';
 if (empty($reseller)) {
