c02e8ebb3aac7b7a968845a5c2797f382138d44e
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-08 12:40:24 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-08 12:40:24

--- reseller/pages/dashboard.php
+++ reseller/pages/dashboard.php
@@ -192,7 +192,7 @@ new Chart(ctx, {
           <th class="px-2 md:px-4 py-2 w-[7%]">No</th>
           <th class="px-2 md:px-4 py-2 w-[30%]">Username</th>
           <th class="px-2 md:px-4 py-2 w-[15%]">Protocol</th>
-          <th class="px-2 md:px-4 py-2 w-[18%]">Expired</th>
+          <th class="px-2 md:px-4 py-2 w-[19%]">Expired</th>
           <th class="px-2 md:px-4 py-2 w-[20%]">UUID/Pass</th>
           <th class="px-2 md:px-4 py-2 w-[10%] hidden md:table-cell">VPS</th>
         </tr>
