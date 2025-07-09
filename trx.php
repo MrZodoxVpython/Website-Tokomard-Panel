da855b27dc34fc5c5d7f6ab641400637cc899695
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-09 17:31:54 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-09 17:31:54

--- trx.php
+++ trx.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/../../koneksi.php';
+require_once __DIR__ . 'koneksi.php';
 
 if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
     echo "Akses ditolak.";
