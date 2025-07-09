78c0f721d74dc6a0964b30ad52051003bae5bde3
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-09 22:46:15 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-09 22:46:15

--- reseller/pages/vless.php
+++ reseller/pages/vless.php
@@ -38,7 +38,7 @@ $sshProducts = [
 ?>
 
 <div class="mb-6">
-    <h2 class="text-2xl font-semibold mb-2">🌐 Daftar Produk Vless</h2>
+    <h2 class="text-2xl font-semibold mb-2">🛰️ Daftar Produk Vless</h2>
     <p class="text-sm text-gray-600 dark:text-gray-400">Pilih produk Vless yang tersedia sesuai lokasi dan kebutuhan Anda.</p>
 </div>
 
