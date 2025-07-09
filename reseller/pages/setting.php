e30bf8a42b108c8c12d2ac0a59701b1199ae4ca7
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-09 11:49:27 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-09 11:49:27

--- reseller/pages/setting.php
+++ reseller/pages/setting.php
@@ -97,7 +97,8 @@ $stmt->close();
         </div>
       </div>
 
-<!-- Tabs -->
+      <div class="flex-1 w-full space-y-4">
+        <!-- Tabs -->
   <div class="max-w-4xl mx-auto px-4 py-8">
     <h1 class="text-2xl font-bold mb-6">Pengaturan Akun</h1>
 
