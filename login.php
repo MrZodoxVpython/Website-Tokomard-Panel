41b11f295fc96e300a7ae71f9f682cb2979d0873
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-12 21:35:41 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-12 21:35:41

--- login.php
+++ login.php
@@ -1,5 +1,15 @@
-<?php session_start(); ?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+include 'koneksi.php';
+require_once 'google-config.php';
+session_start();
+
+$google_login_url = $client->createAuthUrl();
+
+?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
