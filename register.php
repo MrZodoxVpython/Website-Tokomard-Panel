40b31bf0ac0329b20305a59ca0b651d815973e03
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-12 23:10:04 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-12 23:10:04

--- register.php
+++ register.php
@@ -140,7 +140,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 
     <div>
-      <label for="password" class="block text-sm mb-1">Password</label>
+      <label for="password" class="block text-sm mb-1 mt-2">Password</label>
       <input type="password" id="password" name="password" required
              class="w-full px-4 py-2 rounded-md bg-gray-700 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
     </div>
