2544e3cd381c1f5a471253a6bddc1e24ae66c869
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-12 22:19:02 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-12 22:19:02

--- login.php
+++ login.php
@@ -75,10 +75,10 @@ $google_login_url = $client->createAuthUrl();
       <a href="register.php" class="text-blue-400 hover:underline font-semibold">Daftar di sini</a>
     </p>
     <div class="mt-6 text-center">
-      <p class="text-sm mb-2">Atau daftar dengan</p>
-<a href="<?= htmlspecialchars($google_login_url) ?>" class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md">
+<p class="text-sm mb-2">Atau login dengan</p>
+<a href="google-login.php?mode=login" class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md">
   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/120px-Google_%22G%22_logo.svg.png" class="w-5 h-5 mr-2" alt="Google logo">
-  Sign up with Google
+  Sign in with Google
 </a>
 
     </div>
