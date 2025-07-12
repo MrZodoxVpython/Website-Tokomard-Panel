a26cd8b5d5f5f8730a80e57706a4b9700c3fa753
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-12 21:05:22 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-12 21:05:22

--- registerXxX.php
+++ registerXxX.php
@@ -97,7 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <div class="mt-6 text-center">
   <p class="text-sm mb-2">Atau daftar dengan</p>
   <a href="google-login.php" class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" class="w-5 h-5 mr-2" alt="Google logo">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/120px-Google_%22G%22_logo.svg.png" class="w-5 h-5 mr-2" alt="Google logo">
     Sign in with Google
   </a>
 </div>
