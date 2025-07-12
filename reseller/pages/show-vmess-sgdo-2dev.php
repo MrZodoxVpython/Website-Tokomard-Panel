116dcf7ebfe5ffebb2b5c0697c8b24b7b8ce8cdd
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-12 16:32:30 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-12 16:32:30

--- reseller/pages/show-vmess-sgdo-2dev.php
+++ reseller/pages/show-vmess-sgdo-2dev.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             foreach ($lines as $line) {
                 if (preg_match('/^\s*###\s+' . preg_quote($userEdit, '/') . '\s+(\d{4}-\d{2}-\d{2})/', $line, $matches)) {
-                    $expiredLama = $matches[2];
+                    $expiredLama = $matches[1];
                     break;
                 }
             }
