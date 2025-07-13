784ea2efe7ea9e2556c6787f102f7b00325b4937
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-13 13:44:26 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-13 13:44:26

--- register.php
+++ register.php
@@ -213,6 +213,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <input type="password" id="confirm_password" name="confirm_password" required
              class="w-full px-4 py-2 rounded-md bg-gray-700 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
     </div>
+    <button type="button" onclick="kirimOTP()" class="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-md transition">
+      Kirim OTP ke Email
+    </button>
+  </div>
 <!-- Spinner Loading (Letakkan setelah tombol) -->
 <div id="otpLoading" class="hidden flex justify-center mt-4">
   <svg class="animate-spin h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -220,10 +224,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
   </svg>
 </div>
-    <button type="button" onclick="kirimOTP()" class="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-md transition">
-      Kirim OTP ke Email
-    </button>
-  </div>
+
   <!-- Step 2: OTP input -->
   <div id="step2" class="hidden">
     <div>
